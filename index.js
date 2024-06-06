88eef17b7e6decb09b20e3f4bb34c0a84d5d4a4f
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-06-06 20:02:56 +0800

    change to multi-page app, change styling rules

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const main = document.querySelector('main');
 const toggleButton = document.getElementById('navToggle');
 const navList = document.getElementById('navigation-list');
+const navLinks = document.querySelectorAll('.list__item-link');
+
+const setActiveClass = (id) => {
+  const link = document.getElementById(`${id}`);
+  if (main.classList.contains(`container--${id}`)) {
+    link.classList.add('active');
+  }
+};
+
+navLinks.forEach((link) => {
+  setActiveClass(link.id);
+});
 
 toggleButton.addEventListener('click', () => {
   const isExpanded = toggleButton.getAttribute('aria-expanded') === 'true';
@@ -11,4 +24,10 @@ toggleButton.addEventListener('click', () => {
     toggleButton.setAttribute('aria-expanded', 'false');
     navList.removeAttribute('opened');
   }
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    link.classList.add('active');
+  });
 });
