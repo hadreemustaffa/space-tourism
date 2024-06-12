6024d24840e3d8a66597574b17f1b3ab78a4dd46
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-06-12 16:50:46 +0800

    feat: improve tab control
    - centralize all tab control
    - add keyboard navigation

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const navList = document.getElementById('navigation-list');
 const navLinks = document.querySelectorAll('.list__item-link');
 
 // fetch and cache data
-export async function fetchData() {
+async function fetchData() {
   const localStorageKey = 'dataCache';
 
   // Check if data is already in local storage
@@ -32,20 +32,13 @@ export async function fetchData() {
   }
 }
 
-export const setSelectedTab = (element) => {
-  const currentTab = document.querySelector("[aria-selected='true']");
-
-  currentTab?.setAttribute('aria-selected', 'false');
-  currentTab.setAttribute('tabindex', '-1');
-  element.setAttribute('aria-selected', 'true');
-  element.setAttribute('tabindex', '0');
-};
-
+let currentPageId;
 const setActivePage = (id) => {
   const link = document.getElementById(`${id}`);
 
   if (main.classList.contains(`container--${id}`)) {
     link.classList.add('active');
+    currentPageId = id;
   }
 };
 
@@ -70,3 +63,93 @@ navLinks.forEach((link) => {
     link.classList.add('active');
   });
 });
+
+// tab control logic
+const setSelectedTab = (element) => {
+  const currentTab = document.querySelector("[aria-selected='true']");
+
+  currentTab?.setAttribute('aria-selected', 'false');
+  currentTab.setAttribute('tabindex', '-1');
+  element.setAttribute('aria-selected', 'true');
+  element.setAttribute('tabindex', '0');
+};
+
+const data = await fetchData();
+
+if (data && currentPageId != 'home') {
+  const description = document.querySelector('.container__description');
+  const item = document.querySelector('.container__item');
+  const pictureSource = item.querySelector('picture source');
+  const img = item.querySelector('img');
+
+  const tablist = document.querySelector("[role='tablist']");
+  const tabs = tablist.querySelectorAll("[role='tab']");
+  const tabPanel = document.querySelector("[role='tabpanel']");
+  const tabName = document.querySelector('.container__text-name');
+
+  const { destination, crew, technology } = data;
+
+  // change tab content
+  const changeTab = () => {
+    setSelectedTab(tabs[focusIndex]);
+
+    if (currentPageId === 'destination') {
+      const destinationName = document.querySelector('.container__destination');
+      const distance = document.querySelector('.container__distance p');
+      const travelTime = document.querySelector('.container__travel-time p');
+
+      pictureSource.srcset = destination[focusIndex].images.webp;
+      img.src = destination[focusIndex].images.png;
+      destinationName.textContent = destination[focusIndex].name;
+      description.textContent = destination[focusIndex].description;
+      distance.textContent = destination[focusIndex].distance;
+      travelTime.textContent = destination[focusIndex].travel;
+    }
+
+    if (currentPageId === 'crew') {
+      const crewTitle = document.querySelector('.container__text-title');
+
+      pictureSource.srcset = crew[focusIndex].images.webp;
+      img.src = crew[focusIndex].images.png;
+      crewTitle.textContent = crew[focusIndex].role;
+      tabName.textContent = crew[focusIndex].name;
+      description.textContent = crew[focusIndex].bio;
+    }
+
+    if (currentPageId === 'technology') {
+      pictureSource.srcset = technology[focusIndex].images.portrait;
+      img.src = technology[focusIndex].images.landscape;
+      tabName.textContent = technology[focusIndex].name;
+      description.textContent = technology[focusIndex].description;
+    }
+  };
+
+  let focusIndex = 0;
+  const changeTabIndex = (e) => {
+    if (e.key === 'ArrowRight') {
+      focusIndex === tabs.length - 1 ? (focusIndex = 0) : focusIndex++;
+      changeTab();
+    }
+
+    if (e.key === 'ArrowLeft') {
+      focusIndex === 0 ? (focusIndex = tabs.length - 1) : focusIndex--;
+      changeTab();
+    }
+  };
+
+  tabs.forEach((tab, tabIndex) => {
+    tab.addEventListener('click', () => {
+      focusIndex = tabIndex;
+      changeTab();
+    });
+  });
+
+  tablist.addEventListener('keydown', (e) => {
+    changeTabIndex(e);
+    tabs[focusIndex].focus();
+  });
+
+  tabPanel.addEventListener('keydown', (e) => {
+    changeTabIndex(e);
+  });
+}
